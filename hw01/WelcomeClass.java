1524098a38ccfb80f2c31ab6fbf47a166a2e660b
jpb218 <jpb218@lehigh.edu>
2014-09-01 15:00:22 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Justin Brock-Smith
+//8/31/14
+//WelcomeClass hw01
+//cse002
+
+//define the class
+public class WelcomeClass{
+    //add a main method
+    public static void main(String[] args) {
+        
+        //print out this statement
+        System.out.println("    ----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    ----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--P--B--2--1--8-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This summer I worked as a lifeguard at my former highschool. As painful as it was having to spend all my time at a place I was more than happy to leave, I had a good time and made a lot of friends.");
+    }
+}
