349fe5a19ef3c88ad869766eae2bedd3248d0d9e
jpb218 <jpb218@lehigh.edu>
2014-09-26 00:02:10 +0000

    updated lab04-BigMacAgain

--- lab04-BigMacAgain/BigMacAgain.java
+++ lab04-BigMacAgain/BigMacAgain.java
@@ -5,20 +5,25 @@ public class BigMacAgain{
     public static void main(String[] args){
    Scanner bigMac2Input;//creates the scanner
    bigMac2Input = new Scanner(System.in);//sets scanner as next input
-   System.out.print("How many Big Macs did you purchase?");
+   System.out.print("How many Big Macs did you purchase? ");
    //this if else statement finds out whether or not an integer was entered
    if(bigMac2Input.hasNextInt()){
        //setting an integer equal to the number of Big Macs
-        if(BigMac2Input>0){//this is to see whether the number of Big Macs is positive
-        System.out.println("You ordered "+numbBigMacs+" for a total cost of $2.22*"+numbBigMacs+"="+(numbBigMacs*2.22));
-        System.out.print("Would you like fries as well? (Y,y,N,n)");
+       int numbBigMacs=(int) bigMac2Input.nextInt();
+        if(numbBigMacs>0){//this is to see whether the number of Big Macs is positive
+        int firstTotalInt= (int) (100*(numbBigMacs*2.22));
+        double firstTotalDoub=((double) firstTotalInt)/100;
+        System.out.println("You ordered "+numbBigMacs+" for a total cost of $2.22*"+numbBigMacs+"=$"+(firstTotalDoub));
+        System.out.print("Would you like fries as well? (Y,y,N,n) ");
         Scanner bigMac3Input;
-        bigMac3Input.NextString();//sets fries equal to next string
-            if(bigMac3Input.equals("Y"||"y")){
-            System.out.println("Your new total is $"+(numbBigMacs*2.22)+"+$2.15="+(2.15+numbBigMacs*2.22));
+        bigMac3Input = new Scanner(System.in);//sets fries equal to next string
+        
+            if(bigMac3Input.next().equals("Y")||bigMac3Input.next().equals("y")){
+            
+            System.out.println("Your new total is $"+(firstTotalDoub)+"+$2.15=$"+(2.15+firstTotalDoub));
             //this is the output for fries, simply adds that cost to the total cost
-            }else if(bigMac3Input.equals("N"||"n")){
-            System.out.println("Your total cost remains at $"+(numbBigMacs*2.22));
+            }else if(bigMac3Input.next().equals("N")||bigMac3Input.next().equals("n")){
+            System.out.println("Your total cost remains at $"+(firstTotalDoub));
             //this outputs the final cost if you do not get fries
             }else{
             System.out.println("You did not enter Y,y,N or n :(");
